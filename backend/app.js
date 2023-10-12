69da5915a39f7d96145453f86bd1c826c18f9254
danygot18 <danygot74@gmail.com>
2023-10-12 10:02:18 +0700

    new

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const app = express();
 const cookieParser = require('cookie-parser')
-
-app.use(express.json());
-app.use(cookieParser());
 
 const products = require('./routes/product');
 const auth = require('./routes/auth');
 
-app.use('/api/v1',products);
-app.use('/api/v1',auth);
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+app.use('/api/v1', products);
+app.use('/api/v1', auth);
+
+
 module.exports = app
